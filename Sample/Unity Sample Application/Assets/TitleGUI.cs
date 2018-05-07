3c5c7cb5c5d2ae875d3cbf1814ba26aca5897b2d
Christopher Ueda <christopher.ueda@vungle.com>
2018-05-07 16:46:55 -0700

    Update to 6.2.x
    changes to TitleGUI.cs, where the logic is stored

--- Sample/Unity Sample Application/Assets/TitleGUI.cs
+++ Sample/Unity Sample Application/Assets/TitleGUI.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WSA_10_0 || UNITY_WINRT_8_1 || UNITY_METRO
+// To ensure proper behavior of the Vungle SDK, please target an iOS, Android, or Windows platform in the Unity Editor.
 public class TitleGUI : MonoBehaviour {
+#if UNITY_IPHONE || UNITY_ANDROID || UNITY_WSA_10_0 || UNITY_WINRT_8_1 || UNITY_METRO
 
+	// These AppIDs point to Vungle test applications on the dashboard.
+	// Replace these with your own AppIDs to test your app's dashboard settings.
 	string iOSAppID = "5912326f0e96c1a540000014";
 	string androidAppID = "591236625b2480ac40000028";
 	string windowsAppID = "59792a4f057243276200298a";
 
+	// These PlacementIDs point to Vungle test applications on the dashboard.
+	// Replace these with your own PlacementIDs to test your placements' dashboard settings.
 #if UNITY_IPHONE
 	Dictionary<string, bool> placements = new Dictionary<string, bool>
 	{
@@ -33,19 +38,18 @@ public class TitleGUI : MonoBehaviour {
 	};
 #endif
 
-	List<string> placementIdList;
-
 	public Button initSDKButton;
 	public Button playPlacement1Button;
 	public Button loadPlacement2Button;
 	public Button playPlacement2Button;
 	public Button loadPlacement3Button;
 	public Button playPlacement3Button;
-
 	public Text appIDText;
 	public Text placementID1Text;
 	public Text placementID2Text;
 	public Text placementID3Text;
+
+	List<string> placementIdList;
 
 	bool adInited = false;
 
@@ -54,12 +58,12 @@ public class TitleGUI : MonoBehaviour {
 		SetupButtonsAndText();
 	}
 
-	// Update is called once per frame
+
 	void Update () {
 		updateButtonState ();
 	}
 
-	// Called when the player pauses
+
 	void OnApplicationPause(bool pauseStatus) {
 		if (pauseStatus) {
 			Vungle.onPause ();
@@ -68,9 +72,10 @@ public class TitleGUI : MonoBehaviour {
 		}
 	}
 
+
+	// UI initialization
 	void SetupButtonsAndText () {
 		placementIdList = new List<string>(placements.Keys);
-
 		string appID;
 
 #if UNITY_IPHONE
@@ -95,6 +100,9 @@ public class TitleGUI : MonoBehaviour {
 		playPlacement3Button.onClick.AddListener (onPlayPlacement3);
 	}
 
+
+	// Vungle SDK initialization
+	// Uses an AppID for iOS, Android, or Windows depending on platform
 	void onInitButton() {
 		DebugLog("Initializing the Vungle SDK");
 
@@ -116,19 +124,22 @@ public class TitleGUI : MonoBehaviour {
 		initializeEventHandlers ();
 	}
 
+
 	void onPlayPlacement1 () {
 		Vungle.playAd(placementIdList[0]);
 	}
+
 
 	void onLoadPlacement2 () {
 		Vungle.loadAd(placementIdList[1]);
 	}
 
+
 	void onPlayPlacement2 () {
 		// option to change orientation
 		Dictionary<string, object> options = new Dictionary<string, object> ();
 #if UNITY_IPHONE
-options ["orientation"] = 5;
+		options ["orientation"] = 5;
 #else
 		options ["orientation"] = true;
 #endif
@@ -136,9 +147,11 @@ options ["orientation"] = 5;
 		Vungle.playAd(options, placementIdList[1]);
 	}
 
+
 	void onLoadPlacement3 () {
 		Vungle.loadAd(placementIdList[2]);
 	}
+
 
 	void onPlayPlacement3 () {
 		// option to customize alert window and send user_id
@@ -152,6 +165,25 @@ options ["orientation"] = 5;
 		Vungle.playAd(options, placementIdList[2]);
 	}
 
+
+	// Manual implementation of GDPR compliance code
+	// To use, attach this function to a button click event
+	/*
+	void onConsentButton() {
+		if(consentState == Vungle.Consent.Accepted) {
+			consentState = Vungle.Consent.Denied;
+			consentButton.gameObject.GetComponent<Image>().color = Color.red;
+			Vungle.updateConsentStatus(consentState);
+		}	
+		else {
+			consentState = Vungle.Consent.Accepted;
+			consentButton.gameObject.GetComponent<Image>().color = Color.green;
+			Vungle.updateConsentStatus(consentState);
+		}
+	}	
+	*/
+
+
 	void updateButtonState() {
 		playPlacement1Button.interactable = placements[placementIdList[0]];
 		loadPlacement2Button.interactable = adInited & !placements[placementIdList[1]];
@@ -160,31 +192,27 @@ options ["orientation"] = 5;
 		playPlacement3Button.interactable = placements[placementIdList[2]];
 	}
 
-	/* Setup EventHandlers for all available Vungle events */
+
+	// Setup EventHandlers for all available Vungle events
 	void initializeEventHandlers() {
 
-		//Event triggered during when an ad is about to be played
+		// Event triggered during when an ad is about to be played
 		Vungle.onAdStartedEvent += (placementID) => {
 			DebugLog ("Ad " + placementID + " is starting!  Pause your game  animation or sound here.");
 		};
 
-		//Event is triggered when a Vungle ad finished and provides the entire information about this event
-		//These can be used to determine how much of the video the user viewed, if they skipped the ad early, etc.
+		// Event is triggered when a Vungle ad finished and provides the entire information about this event
+		// These can be used to determine how much of the video the user viewed, if they skipped the ad early, etc.
 		Vungle.onAdFinishedEvent += (placementID, args) => {
 			DebugLog ("Ad finished - placementID " + placementID + ", was call to action clicked:" + args.WasCallToActionClicked +  ", is completed view:" 
 				+ args.IsCompletedView);
 		};
 
-		//Event is triggered when the ad's playable state has been changed
-		//It can be used to enable certain functionality only accessible when ad plays are available
+		// Event is triggered when the ad's playable state has been changed
+		// It can be used to enable certain functionality only accessible when ad plays are available
 		Vungle.adPlayableEvent += (placementID, adPlayable) => {
 			DebugLog ("Ad's playable state has been changed! placementID " + placementID + ". Now: " + adPlayable);
 			placements[placementID] = adPlayable;
-		};
-
-		//Fired log event from sdk
-		Vungle.onLogEvent += (log) => {
-			DebugLog ("Log: " + log);
 		};
 
 		//Fired initialize event from sdk
@@ -193,11 +221,25 @@ options ["orientation"] = 5;
 			DebugLog ("SDK initialized");
 		};
 
+		// Other events
+		/*
+		//Vungle.onLogEvent += (log) => {
+			DebugLog ("Log: " + log);
+		};
+
+		Vungle.onPlacementPreparedEvent += (placementID, bidToken) => {
+		    DebugLog ("<onPlacementPreparedEvent> Placement Ad is prepared with bidToken! " + placementID + " " + bidToken);
+		};
+		 
+		Vungle.onVungleCreativeEvent += (placementID, creativeID) => {
+		    DebugLog ("<onVungleCreativeEvent> Placement Ad is about to play with creative ID " + placementID + " " + creativeID);
+		};
+		*/
 	}
 
 	/* Common method for ensuring logging messages have the same format */
 	void DebugLog(string message) {
 		Debug.Log("VungleUnitySample " + System.DateTime.Today +": " + message);
 	}
-}
 #endif
+}
